e1a3d49f634d6271e7af16e9ede292104458189b
abbycarr <carr.ab@husky.neu.edu>
2021-02-04 16:40:21 -0500

    adding app code

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import { useState } from 'react';
+import { uniq, match, test_guess, make_secret_numb } from './game';
+import './App.css';
+
+
+function GameLost(r) {
+  let { reset } = r;
+  return (
+    <div className="App">
+      <h1>You Lost!</h1>
+      <h3>Try again?</h3>
+      <p>
+        <button class="tertiary" onClick={reset}>
+          New Game
+        </button>
+      </p>
+    </div>
+  );
+}
+
+function GameWon(r) {
+  let { reset } = r;
+  return (
+    <div className="App">
+      <h1>You Won!</h1>
+      <h3>Play again?</h3>
+      <p>
+        <button class="tertiary" onClick={reset}>
+          New Game
+        </button>
+      </p>
+    </div>
+  );
+}
+
+function App() {
+
+  const [text, setText] = useState("");
+  const [guesses, setGuesses] = useState([]);
+  const [results, setResults] = useState([]);
+  const [numb, setNumb] = useState(make_secret_numb());
+
+  let turns = 8 - guesses.length;
+
+  function updateText(ev) {
+    let vv = ev.target.value;
+    let cc = vv[vv.length - 1];
+    if (!isNaN(cc) || vv === "") {
+      setText(vv);
+    }
+  }
+
+  function guess() {
+    if (test_guess(text)) {
+      let ng = uniq(guesses.concat(text));
+      setGuesses(ng);
+      let res = match(numb, text);
+      setResults([...results, res]);
+      setText("");
+    }
+  }
+
+  function keyPress(ev) {
+    if (ev.key === "Enter") {
+      guess();
+    }
+  }
+
+  function newGame() {
+    setText("");
+    setGuesses([]);
+    setResults([]);
+    setNumb(make_secret_numb());
+  }
+
+  if (turns <= 0) {
+    return <GameLost reset={newGame} />;
+  }
+
+  if (results[results.length - 1] == "4B0C") {
+    return <GameWon reset={(newGame)} />;
+  }
+
+  return (
+    <div className="App">
+      <h1>Bulls and Cows</h1>
+      <p>Guess the four-digit number. More information on the game&nbsp;
+       <a href="https://en.wikipedia.org/wiki/Bulls_and_Cows" rel="noreferrer" target="_blank">here</a>
+        .
+      </p>
+      <p>
+        Input:&nbsp;
+        <input type="text"
+          value={text}
+          onChange={updateText}
+          onKeyPress={keyPress} />
+        <button class="inverse" onClick={guess}>OK</button>
+      </p>
+      <table max-height="800px">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Guess</th>
+            <th>Result</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td data-label="Count">1</td>
+            <td data-label="Guess">{guesses[0]}</td>
+            <td data-label="Result">{results[0]}</td>
+          </tr>
+          <tr>
+            <td data-label="Count">2</td>
+            <td data-label="Guess">{guesses[1]}</td>
+            <td data-label="Result">{results[1]}</td>
+          </tr>
+          <tr>
+            <td data-label="Count">3</td>
+            <td data-label="Guess">{guesses[2]}</td>
+            <td data-label="Result">{results[2]}</td>
+          </tr>
+          <tr>
+            <td data-label="Count">4</td>
+            <td data-label="Guess">{guesses[3]}</td>
+            <td data-label="Result">{results[3]}</td>
+          </tr>
+          <tr>
+            <td data-label="Count">5</td>
+            <td data-label="Guess">{guesses[4]}</td>
+            <td data-label="Result">{results[4]}</td>
+          </tr>
+          <tr>
+            <td data-label="Count">6</td>
+            <td data-label="Guess">{guesses[5]}</td>
+            <td data-label="Result">{results[5]}</td>
+          </tr>
+          <tr>
+            <td data-label="Count">7</td>
+            <td data-label="Guess">{guesses[6]}</td>
+            <td data-label="Result">{results[6]}</td>
+          </tr>
+          <tr>
+            <td data-label="Count">8</td>
+            <td data-label="Guess">{guesses[7]}</td>
+            <td data-label="Result">{results[7]}</td>
+          </tr>
+        </tbody>
+      </table>
+      <button onClick={newGame}>New Game</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/game.js
@@ -0,0 +1,55 @@
+export function uniq(xs) {
+  return Array.from(new Set(xs));
+}
+
+export function match(numb, guess) {
+
+  let numbers = numb.split('');
+  let goods = guess.split('').filter((x) => numbers.includes(x));
+  let bulls = 0;
+  let cows = goods.length;
+
+  let n = numb.slice();
+  let g = guess.slice();
+
+  for (let i = 0; i < goods.length; i++) {
+    let x = goods[i];
+    if (n.indexOf(x) == g.indexOf(x)) {
+      cows -= 1;
+      bulls += 1;
+    }
+  }
+  return (bulls + "B" + cows + "C");
+}
+
+export function test_guess(text) {
+  if (text.length == 4) {
+    let isUnique = true;
+    let copy = text.slice();
+    let i, j;
+    for (i = 0; i < 4; i++) {
+      for (j = i + 1; j < 4; j++) {
+        if (copy[i] === text[j]) {
+          isUnique = false;
+        }
+      }
+    }
+    if (isUnique) {
+      return true;
+    }
+    else {
+      alert("Your four-digit guess must have four unique digits!");
+      return false;
+    }
+  }
+  else {
+    alert("Your guess must be a four-digit number!");
+    return false;
+  }
+}
+
+export function make_secret_numb() {
+  let numbs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  numbs = numbs.sort(() => Math.random() - 0.5);
+  return ("" + numbs[0] + numbs[1] + numbs[2] + numbs[3]);
+}
